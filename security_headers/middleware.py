b6831f67784a040aa179d950e8417bab266bb3a0
rogue707dev <rogue901126@protonmail.com>
2019-06-20 15:28:59 -0400

    Added framing allowed and referrer-policy

---
+++ security_headers/middleware.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+
+
+def resp_set_headers_middleware(get_response):
+    """
+    Sets all wanted security headers common to all responses.
+    """
+
+    def middleware(req):
+        resp = get_response(req)
+
+        iframe_allowed_from = settings.get("FRAMING_ALLOWED_FROM", [])
+        if iframe_allowed_from:
+            resp["X-Frame-Options"] = "allow-from {}".format(
+                " ".join(iframe_allowed_from)
+            )
+
+        resp["Referrer-Policy"] = "same-origin"
+
+        return resp
+
+    return middleware
